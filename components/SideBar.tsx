2ae0f7233711ed71c313bab211050dfb33241bf1
GrungeElFz <elf0114_@hotmail.com>
2023-06-27 11:42:50 -0500

    Modified:   Added `pathname` and `routes` (components/SideBar.tsx)

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
+import { usePathname } from "next/navigation";
 
 interface SideBarProps {
   children: React.ReactNode;
 }
 
 const SideBar: React.FC<SideBarProps> = ({ children }) => {
+  const pathname = usePathname();
+  const routes = useMemo(
+    () => [
+      {
+        label: "Home",
+        active: pathname !== "search",
+        href: "/",
+      },
+      {
+        label: "Search",
+        active: pathname === "search",
+        href: "/search",
+      },
+    ],
+    [pathname]
+  );
+
   return <div>{children}</div>;
 };
 
