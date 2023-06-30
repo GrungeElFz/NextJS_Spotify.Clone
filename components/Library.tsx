8023af0e6e4320da0f23884b0c3e0691c483bf12
GrungeElFz <elf0114_@hotmail.com>
2023-06-30 13:02:55 -0500

    Modified:   Added comments (components/Library.tsx)

--- components/Library.tsx
+++ components/Library.tsx
@@ -11,16 +11,21 @@ const Library = () => {
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
+        {/* Your Library */}
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist size={26} className="text-neutral-400" />
           <p className="text-neutral-400 font-medium text-md">Your Library</p>
         </div>
+
+        {/* Add Icon */}
         <AiOutlinePlus
           size={20}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
           onClick={onCLick}
         />
       </div>
+
+      {/* List of Songs */}
       <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs</div>
     </div>
   );
