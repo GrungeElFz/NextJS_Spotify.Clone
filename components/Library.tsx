fb9a542c0e3b5d62bc4eec1f93397700d093b2f1
GrungeElFz <elf0114_@hotmail.com>
2023-06-30 12:47:36 -0500

    Modified:   Import { TbPlaylist } from "react-icons/tb" (components/Library.tsx)             Replaced `<h1>` 'Library' by `<TbPlaylist .. />`

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { TbPlaylist } from "react-icons/tb";
+
 const Library = () => {
   const onCLick = () => {
     // Handle Upload
   };
 
   return (
-    <div>
-      <h1>Library</h1>
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+        </div>
+      </div>
     </div>
   );
 };
